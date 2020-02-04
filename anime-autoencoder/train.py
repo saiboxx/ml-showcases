a8956c5c5157753ada057277c20c639d9a28b5ae
Tobias Weber <tobias.weber.1@web.de>
2020-02-04 19:24:31 +0100

    Added KL annealing

--- anime-autoencoder/train.py
+++ anime-autoencoder/train.py
@@ -4,9 +4,9 @@ from dataset import AnimeFaceDataset
 from autoencoder import VAE, Encoder, Decoder, ConvEncoder, ConvDecoder
 import torch
 from torch import tensor
-from torch.nn import BCELoss
+from torch.nn import MSELoss
 from torch.optim import Adam
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import utils
 from torchsummary import summary
 import matplotlib.pyplot as plt
@@ -23,7 +23,7 @@ print("Utilizing device {}.".format(device))
 
 def train():
 
-    data = AnimeFaceDataset("data")
+    data = AnimeFaceDataset("data2")
     dataloader = DataLoader(data,
                             batch_size=args.batch_size,
                             shuffle=True)
@@ -43,34 +43,46 @@ def train():
     autoencoder.to(device)
 
     optimizer = Adam(autoencoder.parameters(), lr=0.001)
-    bce_loss = BCELoss()
+    mse_loss = MSELoss()
 
     # Print model summary
     summary(autoencoder, input_size=(3, 64, 64))
+
+    kl_weight = 0
 
     for e in range(1, args.epochs + 1):
         for i_batch, sample in enumerate(dataloader):
             x, mu, logvar = autoencoder(sample.to(device))
 
             # Reconstruction Loss
-            rec_loss = bce_loss(x, sample.to(device))
+            rec_loss = mse_loss(x, sample.to(device))
 
             # Kullback-Leibler Divergence
-            kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+            kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) / (64 * 64)
 
             # Total Loss
-            loss = rec_loss + kl_loss
+            loss = rec_loss + kl_weight * kl_loss
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             if i_batch % 100 == 0:
-                print("Ep. {0:>3} with {1:>5} batches; {2:5.2f} loss".format(e, i_batch, loss))
+                print("Ep. {0:>3} with {1:>5} batches; {2:5.2f} total loss;"
+                      "{3:5.2f} rec. loss; {4:5.2f} kl loss"
+                      .format(e, i_batch, loss, rec_loss, kl_loss))
 
         with torch.no_grad():
+            # Save generated images
             rand = (-1 - 1) * torch.rand([100, 64]).to(device) + 1
-            save_images(autoencoder.decoder(rand), e)
+            save_images(autoencoder.decoder(rand), e, "generated")
+            # Save reconstructed images
+            images = torch.stack(data[:100]).to(device)
+            rec_images, mu, logvar = autoencoder(images)
+            save_images(rec_images, e, "reconstructed")
+
+        # Anneal weight of KL-divergence
+        kl_weight = get_kl_weight(e)
 
         # Save models
         os.makedirs("models", exist_ok=True)
@@ -89,7 +101,7 @@ def plot_images(images: list):
     plt.show()
 
 
-def save_images(images: tensor, episode: int):
+def save_images(images: tensor, episode: int, marker: str):
     dir = "test_images/"
     os.makedirs(dir, exist_ok=True)
     plt.figure()
@@ -97,8 +109,20 @@ def save_images(images: tensor, episode: int):
     plt.imshow(grid.cpu().numpy().transpose((1, 2, 0)))
     plt.axis('off')
     plt.ioff()
-    plt.savefig(dir + str(episode) + "_episode.png")
+    plt.savefig(dir + str(episode) + "_episode_" + marker + ".png")
     plt.close()
+
+
+def get_kl_weight(epoch: int) -> float:
+    min_epoch = 10
+    max_epoch = 30
+
+    if epoch < min_epoch:
+        return 0
+    elif epoch >= max_epoch:
+        return 1
+    else:
+        return (epoch - min_epoch) / (max_epoch - min_epoch)
 
 
 if __name__ == '__main__':
