7a1c56c69445b0bdfd55e5f78c662557a865b424
Tobias Weber <tobias.weber.1@web.de>
2020-08-17 17:56:31 +0200

    Added wandb tracking

--- sinus-bigan/bigan.py
+++ sinus-bigan/bigan.py
@@ -1,11 +1,17 @@
 from collections import OrderedDict
+from typing import Optional, Callable
 
+import matplotlib
+
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.metrics.functional import accuracy
 from itertools import chain
 import torch
 import torch.nn as nn
 from torch import tensor
-from torch.optim import Adam
+from torch.optim import Adam, Optimizer
 from torch.distributions.normal import Normal
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
@@ -218,9 +224,11 @@ class BiGAN(pl.LightningModule):
         self.fake_label = torch.zeros(batch_size).cuda()
         self.bce_loss = nn.BCELoss()
 
-
-    def sample_normal(self):
-        z = self.distribution.sample([self.hparams.batch_size, self.hparams.latent_size]).unsqueeze_(-1)
+    def sample_normal(self, size: Optional[int] = None):
+        if size is None:
+            z = self.distribution.sample([self.hparams.batch_size, self.hparams.latent_size]).unsqueeze_(-1)
+        else:
+            z = self.distribution.sample([size, self.hparams.latent_size]).unsqueeze_(-1)
         if self.on_gpu:
             z = z.cuda()
         return z
@@ -236,7 +244,7 @@ class BiGAN(pl.LightningModule):
 
     def configure_optimizers(self):
         generator_opt = Adam(chain(self.generator.parameters(), self.encoder.parameters()))
-        discriminator_opt = Adam(self.discriminator.parameters())
+        discriminator_opt = Adam(self.discriminator.parameters(), lr=1e-4)
         return [generator_opt, discriminator_opt]
 
     def training_step(self, batch, batch_nb, optimizer_idx):
@@ -249,46 +257,81 @@ class BiGAN(pl.LightningModule):
         disc_enc = self.discriminator(x, z_hat)
         disc_gen = self.discriminator(x_hat, z)
 
+        metric_dict = {}
+
         if optimizer_idx == 0:
             # Generator Loss
             loss = self.bce_loss(disc_gen, self.real_label) + self.bce_loss(disc_enc, self.fake_label)
+            metric_dict.update({'Gen Loss': loss})
         else:
             # Discriminator Loss
             loss = self.bce_loss(disc_enc, self.real_label) + self.bce_loss(disc_gen, self.fake_label)
+            metric_dict.update({'Disc Loss': loss})
+
+        acc_fake = torch.where(disc_gen < 0.5, tensor(1).cuda(), tensor(0).cuda()).float().mean()
+        acc_true = torch.where(disc_gen < 0.5, tensor(0).cuda(), tensor(1).cuda()).float().mean()
+
+        metric_dict.update({
+            'Fake Accuracy': acc_fake,
+            'True Accuracy': acc_true
+        })
 
         output = OrderedDict({
             'loss': loss,
-            #'progress_bar': {'Loss': 0},
-            'log': {'Loss': loss}
+            'log': metric_dict
         })
         return output
 
+    def training_epoch_end(self, training_step_outputs):
+        with torch.no_grad():
+            z = self.sample_normal(8)
+            x_hat = self.generator(z).cpu().numpy()
+
+        rows = len(x_hat)
+        fig = plt.figure()
+        for i in range(rows):
+            ax = plt.subplot(rows, 1, i + 1)
+            plt.plot(x_hat[i, 0, :])
+            ax.axis('off')
+
+        self.logger.experiment.log({'Generator': plt})
+        plt.close()
+        return {}
+
+    def optimizer_step(
+            self,
+            epoch: int,
+            batch_idx: int,
+            optimizer: Optimizer,
+            optimizer_idx: int,
+            second_order_closure: Optional[Callable] = None,
+            on_tpu: bool = False,
+            using_native_amp: bool = False,
+            using_lbfgs: bool = False):
+
+        if optimizer_idx == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if optimizer_idx == 1:
+            if batch_idx % 4 == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+
 
 if __name__ == '__main__':
-    # latent_size = 32
-    # dist = Normal(0, 1)
-    # gen = Generator(latent_size=latent_size)
-    # enc = Encoder(latent_size=latent_size)
-
-    # sample = dist.sample([5, latent_size]).unsqueeze_(-1)
-
-    # generated = gen(sample)
-    # print(generated.shape)
-
-    # encoded = enc(generated)
-    # print(encoded.shape)
-
-    # dis = Discriminator(latent_size=latent_size)
-    # trueness = dis(generated, sample)
-    # print(trueness)
     bigan = BiGAN(
-        batch_size=64,
+        batch_size=32,
         sin_range=25,
         seq_length=95,
         latent_size=32,
         num_workers=4
     )
 
+    wandb_logger = WandbLogger(project='sinus-bigan')
+    wandb_logger.watch(bigan, log='gradients', log_freq=100)
+
     trainer = pl.Trainer(gpus=1,
-                         max_epochs=100)
+                         max_epochs=100,
+                         logger=wandb_logger)
     trainer.fit(bigan)
